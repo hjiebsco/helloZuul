b6d8ed68fd8f97eb75f6e3fab509e8950a403418
Hongwei Ji <hji@ebsco.com>
2016-08-05 10:21:33 -0400

    disable some code for now

--- src/main/java/com/hji/play/filters/pre/SimplePostFilter.java
+++ src/main/java/com/hji/play/filters/pre/SimplePostFilter.java
@@ -33,9 +33,11 @@ public class SimplePostFilter extends ZuulFilter {
 	public Object run() {
 		log.info("doing POST filter");
 		RequestContext ctx = RequestContext.getCurrentContext();
-		if (!"/".equals(ctx.getRequest().getContextPath())) {
-			return null;
-		}
+//		ctx.getZuulResponseHeaders().get("requestURI")
+//		System.out.println("[" + ctx.getRequest().getContextPath() + "]");
+//		if (!"/".equals(ctx.getRequest().getContextPath())) {
+//			return null;
+//		}
 		String originResponseBody = "";
 		try {
 			originResponseBody = IOUtils.toString(ctx.getResponseDataStream());

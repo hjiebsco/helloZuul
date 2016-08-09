2d6e14b7903307a7cb408b3312432e8cda956f91
Hongwei Ji <hji@ebsco.com>
2016-08-09 15:37:16 -0400

    test

--- src/main/java/com/hji/play/HelloZuulApplication.java
+++ src/main/java/com/hji/play/HelloZuulApplication.java
@@ -14,6 +14,7 @@ import com.hji.play.filters.pre.SimpleRouteFilter;
 public class HelloZuulApplication {
 
 	public static void main(String[] args) {
+		System.out.println("a new version");
 		SpringApplication.run(HelloZuulApplication.class, args);
 	}
 
